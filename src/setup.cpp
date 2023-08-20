7843564a2bcea96000a08deaf497418e4656d9a9
Jens Torgeir Solaugsten <torgeir@solaugsten.no>
2023-08-20 23:30:37 +0200

    Repo Initialization

---
+++ src/setup.cpp
@@ -0,0 +1,12 @@
+﻿// LearningOpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "setup.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
